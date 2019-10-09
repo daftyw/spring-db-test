833c418e0b944d11d5c12d853bfae251a4539fa6
Rawin Ngamloet <windx7@gmail.com>
2019-10-09 07:28:35 +0700

    Should test any db now

--- src/main/java/windx7/SpringStarterServer.java
+++ src/main/java/windx7/SpringStarterServer.java
@@ -1,6 +1,11 @@
 package windx7;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringStarterServer {
-
+    public static void main (String[] args) {
+        SpringApplication.run(SpringStarterServer.class, args);
+    }
 }
